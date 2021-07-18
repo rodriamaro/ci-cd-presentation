ce3f42f1cd9e2135e2b5999b41c6de12e1825a60
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2021-07-17 22:06:44 -0400

    updating doc

---
+++ main.js
@@ -0,0 +1,12 @@
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+import './style.css'
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({
+  width: "100%",
+  height:"100%",
+  hash: true, 
+  slideNumber: true,
+})
