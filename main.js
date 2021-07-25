e2599e89c340624192b1d9b77ad0ddf0e506dfad
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2021-07-25 01:17:08 -0400

    update with new material

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
+import 'reveal.js/plugin/highlight/monokai.css'
 import './style.css'
 import Reveal from 'reveal.js'
+import Notes from 'reveal.js/plugin/notes/notes.esm.js'
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js'
+import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js'
 
 const deck = new Reveal()
 deck.initialize({
@@ -9,4 +14,5 @@ deck.initialize({
   height:"100%",
   hash: true, 
   slideNumber: true,
+  plugins: [ Markdown, Notes, Highlight, Zoom ]
 })
